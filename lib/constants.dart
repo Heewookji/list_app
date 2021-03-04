17966eaa980757eb142a210cf07310c12a103f1a
heewookji <wlgmldnr@daum.net>
2021-03-04 12:48:14 +0900

    feat: login screen layout 적용

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,5 +4,5 @@ class Constants {
   static const int adsLimit = 10;
   static const int adsInterval = 4;
   static const String imgBaseUrl = 'https://cdn.comento.kr/assignment/';
-  static const double bodyWidthPadding = 0.04;
+  static const double bodyWidthPadding = 0.06;
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -44,33 +44,95 @@ class _LoginScreenState extends State<LoginScreen> {
     _theme = Theme.of(context);
     _screenSize = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: Text('List App'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(
-            horizontal: _screenSize.width * Constants.bodyWidthPadding),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Form(
-              key: _formKey, //assigning key to form
-              child: TextFormField(
-                controller: _controller,
-                autofocus: true,
-                validator: _validator,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.person, color: Colors.grey),
-                ),
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Container(
+            color: Colors.green,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: _screenSize.width * Constants.bodyWidthPadding),
+            child: Container(
+              height: _screenSize.height * 0.3,
+              padding: EdgeInsets.all(_screenSize.width * 0.08),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.2),
+                    spreadRadius: 1,
+                    blurRadius: 8,
+                    offset: Offset(0, 3), // changes position of shadow
+                  ),
+                ],
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    'List App',
+                    style: _theme.textTheme.subtitle1,
+                  ),
+                  _buildForm(),
+                  _buildFlatButton(),
+                ],
               ),
             ),
-            FlatButton(
-              onPressed: () => _submit(),
-              child: _isLoading ? CircularProgressIndicator() : Text('Login'),
-            )
-          ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Form _buildForm() {
+    return Form(
+      key: _formKey, //assigning key to form
+      child: Container(
+        margin: EdgeInsets.only(top: _screenSize.height * 0.02),
+        height: 80 > _screenSize.height * 0.09 ? 80 : _screenSize.height * 0.09,
+        child: TextFormField(
+          controller: _controller,
+          style: _theme.textTheme.bodyText1,
+          validator: _validator,
+          decoration: InputDecoration(
+            labelText: '닉네임을 입력해주세요',
+            labelStyle: TextStyle(color: Colors.grey),
+            prefixIcon: Icon(Icons.person, color: Colors.grey),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              borderSide: BorderSide(color: Colors.teal),
+            ),
+            floatingLabelBehavior: FloatingLabelBehavior.never,
+            contentPadding: EdgeInsets.symmetric(horizontal: 30),
+          ),
         ),
       ),
+    );
+  }
+
+  FlatButton _buildFlatButton() {
+    return FlatButton(
+      height: 40 > _screenSize.height * 0.05 ? 40 : _screenSize.height * 0.05,
+      onPressed: () => _submit(),
+      color: Colors.green,
+      shape: RoundedRectangleBorder(
+        borderRadius: new BorderRadius.circular(10.0),
+      ),
+      child: _isLoading
+          ? SizedBox(
+              height: 20,
+              width: 20,
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+              ),
+            )
+          : Text(
+              '로그인',
+              style: TextStyle(color: Colors.white),
+            ),
     );
   }
 }
